646b67acc78900e8c219f4cd154ee013ea8f858a
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-05-18 16:22:44 +1200

    Added validation to ensure that the study topic was valid & added validation to ensure that the study duration was valid.

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 let studySession = {
-    subject: "x",
+    subject: 'x',
     duration: 1
+}
+const validation = () => {
+    const errors = [];
+    if (studySession === null || typeof studySession !== 'object') {
+        return {ok: false, errors: ['studySession must be an object.']};
+    }
+    if (typeof studySession.subject !== 'string') {
+        errors.push('Subject must be a string.')
+    }
+    if (subject === '') {
+        errors.push('Subject cannot be empty.')
+    }
+    if (subject.includes(' ')) {
+        errors.push('Subject cannot have whitespace.');
+    }
+    if (typeof studySession.duration !== 'number' || (!Number.isInteger(duration))) {
+        errors.push('Duration must be a whole number.');
+    } else if (studySession.duration.length <= 0) {
+        errors.push('Duration cannot be zero or less.')
+    }
+    if (errors.length > 0) {
+        return {ok: false, errors: errors};
+    }
+    return {ok: true, value: studySession}; 
 }
